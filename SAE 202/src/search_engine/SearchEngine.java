3c3756acecf25a66eb83d413b966ec14b99177a6
Pirrr3 <pierregaudin33@hotmail.fr>
2023-02-06 11:25:55 +0100

    SearchEngine coquille

---
+++ SAE 202/src/search_engine/SearchEngine.java
@@ -0,0 +1,30 @@
+package search_engine;
+
+
+public class SearchEngine {
+
+	private Path indexation_directory ;
+	private IndexedPages[] pages;
+	
+	
+	public void SearchEngine(Path indexation_directory){
+		
+	}
+	
+	public IndexedPage getPage(int i) {
+		
+	}
+	
+	public int getPagesNumber() {
+		
+	}
+	
+	public SearchResult[] launchRequest(String requestString) {
+		
+	}
+	
+	public void printResults(String requestString) {
+		
+	}
+	
+}
